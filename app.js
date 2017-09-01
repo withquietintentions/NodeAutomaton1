124c29073f112f1ca8a6bb879f9d137e0c341ebb
withquietintentions <Zoe@penelope.local>
2017-08-31 21:34:58 -0700

    Updated stuff

--- app.js
+++ app.js
@@ -4,15 +4,77 @@ var http = require('http');
 var fs = require('fs');
 
 var express = require ("express");//module that allows this to work as a server that finds all dependent files
-var cors = require("cors");
-var app = express();
+var json = require('express-json');
+var cors  = require("cors"); var app = express();
 
+
+var Datastore = require('nedb');
 
 app.use(express.static("./public"));
-
 app.use(cors());
 
 
+var bodyParser = require('body-parser')
+app.use(bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
+
+var dbGuesses = new Datastore({ filename: './db/guesses.db', autoload: true });
+app.post("/guess", function(req, res) {
+    var guess = req.body.guess;
+
+    var doc = {};
+    var guessCleanedUp = guess.trim().toLowerCase();
+
+
+    doc["guess"] = guessCleanedUp;
+    doc["time"] = new Date();
+
+    counter = {};
+    
+    dbGuesses.insert(doc, function(err, updatedDoc) {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      dbGuesses.find({}, function(err, docs) {
+        if (err) {
+          console.log(err);
+          return;
+        } 
+        for (var i = 0; i < docs.length; i++) {
+          var val = docs[i].guess;
+          if (counter[val] == null) {
+            counter[val] = 1;
+          } else {
+            counter[val] +=1;
+          }
+          console.log(counter);
+          var freqs = getTopTenFromCounter(counter);
+          console.log(freqs);
+          
+
+        }
+      })
+    });
+  }
+);
+
+
+function getTopTenFromCounter(counter) {
+  var freqs = {};
+  var counters = Object.keys(counter);
+  for (var i = 0; i < counters.length; i++) {
+    var occurances = counter[counters[i]];
+    if (freqs[occurances] == null) {
+      freqs[occurances] = [counters[i]];
+    } else {
+      freqs[occurances].push(counters[i]);
+    }
+  }
+  return freqs;
+}
 
 app.listen(3000);//tells me I can go to localhost 3000 to find my page in the browser
 
@@ -20,9 +82,5 @@ console.log("Express app running on port 3000");//terminal log
 console.log("this");
 
 module.exports = app;
-
-// var timeStamp = Math.floor(Date.now());
-
-// function(guess, timeStamp){
 
 
--- public/js/dictionary.js
+++ public/js/dictionary.js
@@ -1,95 +1,10 @@
-//Automaton
-// Persistent datastore with manual loading
-// $eq =   Matches values that are equal to a specified value.
-// $gt >   Matches values that are greater than a specified value.
-// $gte    >=  Matches values that are greater than or equal to a specified value.
-// $lt <   Matches values that are less than a specified value.
-// $lte    <=  Matches values that are less than or equal to a specified value.
-// $ne !=  Matches all values that are not equal to a specified value.
-// $in in(x,y..)   Matches any of the values specified in an array.
-// $nin    not in(x,y,…)   Matches none of the values specified in an array.
 
-// console.log ("wtf");
-// // Writing...
-// var fs = require("fs");
-// var myJson = {
-//     key: "myvalue"
-// };
+var dictionary = {
+  onRemove: function(ev) {
+    console.log("called");
+    console.log(ev);
+    var guess = $("#guess").val();
+    $.post("/guess", {guess: guess});
+  }
+};
 
-// fs.writeFile( "filename.json", JSON.stringify( myJson ), "utf8", yourCallback );
-
-// // And then, to read it...
-// //myJson = require("./filename.json");
-
-// //testing read file
-// var topten = fs.readFileSync("../data/guesses.js", "UTF-8")
-// ///starts jquey listening
-// $( document ).ready(function() {
-//     console.log( "ready!" );
-// });
-
-// //what happens on click of submit button
-// //submit on button
-// $("#submission"). submit(function( event ) {
-//     alert ("Handler for .submit() called.");
-//     event.preventDefault();
-
-// });
-
-
-
-// to check against another thing...
-// $( "form" ).submit(function( event ) {
-//   if ( $( "input:first" ).val() === "correct" ) {
-//     $( "span" ).text( "Validated..." ).show();
-//     return;
-//   }
-
-//get input and save as a variable
-
-//append this variable to a list
-
-//count the items in a list to see the frequency of a word and make a topten list
-
-//check the saved varaible against the top ten list
-
-//if equal 
-    //return a yes in the responseMessage
-    //send message to photon to move motor
-
-//if not 
-    //retun a try again in the responseMessage
-
-
-
-
-    // $("#submit").click(function(){
-
-    //     $("#responseMessage").prepend("Good Try!");
-
-    // });
-  
-
-
-//     $.getJSON('/guesses-api', printTerms);
-//     $('form').submit(function (e) {
-//         e.preventDefault();
-//         $.post('/guesses-api', {word: $('#word').val()}, printTerms);
-//         this.reset();
-//     });
-
-// });
-
-// function printTerms(words) {
-//     $('body>dl').empty();
-//     $.each(words, function () {
-//         $('<dl>').text(this.word).appendTo('body>dl');
-//     });
-//     $('dl').off('dblclick').dblclick(function() {
-//         $.ajax({
-//             url: '/guesses-api/' + $(this).text(),
-//             type: 'DELETE',
-//             success: printTerms
-//         });
-//     });
-// }
