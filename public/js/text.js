7431bbdec846121009426a3f0d7726e910a96437
withquietintentions <Zoe@penelope.local>
2017-09-08 21:39:59 -0700

    dicked around with index of to no success

--- public/js/text.js
+++ public/js/text.js
@@ -21,16 +21,16 @@
 
 
 var statements = statements_lose_version;
+var num = 0;
 var question_num = 0;
-var answer = (statements[question_num]["question"]["synopsis"]); //is a string
+var answer = statements[question_num]["question"]["synopsis"]; //is a string
 var optionsA = statements[question_num]["question"]["optionsA"];// options is a list 
 var optionsN = statements[question_num]["question"]["optionsN"];// options is a list 
 var lose_message = statements[question_num]["question"]["lose_message"];// is a string
 var total_questions = statements_lose_version.length;
 var score= 0;
 var lets_play = null;
-
-//var num = 0;
+var num1 = 1;
 var $question = $('#question_synopsis');
 var $optionsA = $('#optionsA');
 var $optionsN = $('#optionsN');
@@ -38,28 +38,28 @@ var $playAgain = $("#play_again");
 var $loseMessage = $("#lose_message");
 var $urnOpening = $("#urn_opening")
 var $optionsli = $('#options');
-
+var play = true ;
+var result = statements_lose_version.indexOf("question", 1);
    
- $( document ).ready(function OpeningPage(num, question_num) {
-	"use strict";
-	var question_num = 0;
-	var play = true ;
+ // $( document ).ready(function OpeningPage() {
+	// "use strict";
+	// $question.text(answer);
+	// //var question_num = 0;
+	// });
 
-	$(".container").click(function(){//temp testing to switch
-	window.location.href = "http://localhost:3000/";//sends it back to other game
-	});
-
-
-	if (play == true) 
-	{
-	var num = 0;
-	var num1 = 0;
+///where state change betweeen opened and closed will occur///
+// $("#button").click(function ChangeState(question_num, num, num1){//temp testing to switch
+// 	window.location.href = "http://localhost:3000/";//sends it back to other game ('port', (process.env.PORT || 3000));
+// 	});
+// ///
+function Test(question_num, num, num1){ 
+	if (play == true) {
 	do{	
 	var enter_negative = optionsA[num];
 	$optionsli.append('<li>'+enter_negative+'</li>');
 	num++;
+	return num
 	}
-	
 	while (num<optionsA.length);
 	do{	
 	var enter_affirmative = optionsN[num1];
@@ -67,8 +67,9 @@ var $optionsli = $('#options');
 	num1++;
 	}
 	while (num1<optionsN.length);
-	}
-	
-	
-});
+}
+}
+
+//ChangeState(question_num, num, num1);
+Test(question_num, num, num1);
 
