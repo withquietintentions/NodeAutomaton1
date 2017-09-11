93d166f9c8c5e5d5c228c8302ad696181146ca1b
withquietintentions <Zoe@penelope.local>
2017-09-10 21:35:56 -0700

    particle talks to app successfully

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 // Automaton
 // Node
+require('dotenv').config();//getting token and id for api
 var http = require('http');
 var fs = require('fs');
-
 var express = require ("express");//module that allows this to work as a server that finds all dependent files
 var json = require('express-json');
 var cors  = require("cors"); var app = express();
@@ -17,30 +17,50 @@ app.use(cors());
 
 //TESTING PARTICLE IN HERE
 //NodeJS code for using Photon variable
-  particle.getVariable({ deviceId: d_id, name: 'int_val', auth: l_token }).then(
+var Particle = require('particle-api-js');
+var particle = new Particle();
+var token = process.env.TOKEN;
+var d_uid = process.env.D_UID;
+
+
+var timer1;
+
+timer1 = setInterval(talk_with_photon,10000);
+
+function talk_with_photon() {
+    g_var(d_uid, token);
+    c_func(d_uid, token);          
+}
+
+function g_var(d_id,l_token){
+    particle.getVariable({ deviceId: d_id, name: 'int_val', auth: l_token }).then(
         function (data) {
             console.log('Device variable retrieved successfully:', data.body.result);
         }, function (err) {
             console.log('An error occurred while getting attrs:', err);
         });
-  //NodeJS code for Cloud function
-var fnPr = particle.callFunction({ deviceId: d_id, name: 'int_fun', argument: 'wifi_rssi', auth: l_token });
+}
 
-fnPr.then(
+function c_func(d_id, l_token) {
+
+    var fnPr = particle.callFunction({ deviceId: d_id, name: 'int_fun', argument: 'wifi_rssi', auth: l_token });
+
+    fnPr.then(
         function (data) {
             console.log('Function called succesfully WiFi_RSSI: ', data.body.return_value);
         }, function (err) {
             console.log('An error occurred:', err);
         });
+        
+    var fnPr2 = particle.callFunction({ deviceId: d_id, name: 'int_fun', argument: 'blink', auth: l_token });
 
-var fnPr2 = particle.callFunction({ deviceId: d_id, name: 'int_fun', argument: 'blink', auth: l_token });
-
-fnPr2.then(
+    fnPr2.then(
         function (data) {
             console.log('Function called succesfully LED: ', data.body.return_value);
         }, function (err) {
             console.log('An error occurred:', err);
         });
+}
 ///END OF PARICLE TEST
 
 
