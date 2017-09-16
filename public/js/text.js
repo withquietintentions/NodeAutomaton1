f9db9c654a604f96dabd658a1a07a87b001f4f2b
withquietintentions <Zoe@penelope.local>
2017-09-15 21:46:52 -0700

    add questions to json

--- public/js/text.js
+++ public/js/text.js
@@ -1,128 +1,73 @@
+$.get('/questions.json', function(data) {
+	console.log(data);
+	var statements_lose_version = data;
+	startLoop(statements_lose_version);
+})
+
+function startLoop(statements_lose_version) {
+	var question_num = 0;
+
+	var $question = $('#question_synopsis');
+	var $optionsA = $('#optionsA');
+	var $optionsN = $('#optionsN');
+	var $playAgain = $("#play_again");
+	var $loseMessage = $("#lose_message");
+	var $urnOpening = $("#urn_opening")
+	var $options = $('#options');
+
+	$playAgain.click(function() {
+		incrementQuestionNum();
+		loadQuestion()
+	});
+
+	$optionsN.click(function() {
+		incrementQuestionNum();
+		loadQuestion()
+	});
+
+	$optionsA.click(function(){
+		$question.empty();
+		$optionsA.empty();
+		$optionsN.empty();
+		var lose_message = statements_lose_version[question_num]["question"]["lose_message"];// is a string
+		$loseMessage.text(lose_message);
+		$urnOpening.text("urn opening...");
+		$playAgain.text("play again");
+	});
 
 
- var statements_lose_version = [  
- 	{"question" : { 
-		"synopsis": "Do you know who created Pandora?",
-		"optionsA" : [ "yes","maybe"],
-		"optionsN" : [ "no", "nah"],
-		"lose_message": "Pandora was created by Hephastios, a metal smith known for his Automatons (metal sculptures who could think and act)."
-	}}, 
-	{"question" : {
-		"synopsis": "Do you know why the Gods came to Hephastos to build her?",
-		"optionsA": [ "no, tell me"],
-		"optionsN": ["I don't care. "],
-		"lose_message": "Becasue he was an awesome hardware dude, but also because man had stole fire and they worried this was elevating man to god like status."
-	}}, 
-	{"question" : {
-		"synopsis": "The main trouble with cyborgs is that they are the illegitimate offspring of militarism and patriarchal capitalism, but illegitimate offspring are...",
-		"optionsA": ["open to read rest"],
-		"optionsN": ["no thanks"],
-		"lose_message": "often exceedingly unfaithful to their origins. Their fathers, after all, are inessential - Donna Haraway A Cyborg Manifesto, 1984."
-	}},
-	{"question" : {
-		"synopsis": "Closer at hand and perhaps equally decisive is another no less threatening event. This is the advent of automation, which in a few decades probably will empty the factories and liberate mankind from its oldest and most natural burden, the burden of laboring and the bondage to necessity. Here, too, a fundamental aspect of the human condition is at stake, but the rebellion against it, the wish to be liberated from labor's -toil and trouble,- is not modern but as old as recorded history. ",
-		"optionsA": ["who said this?"],
-		"optionsN": ["who care?"],
-		"lose_message": "Hannah Ardent, The Human Condition-1958"
-	}}, 
-	{"question" : {
-		"synopsis": "The main trouble with cyborgs is that they are the illegitimate offspring of militarism and patriarchal capitalism, but illegitimate offspring are...",
-		"optionsA": ["open to read rest"],
-		"optionsN": ["no thanks"],
-		"lose_message": "often exceedingly unfaithful to their origins. Their fathers, after all, are inessential - Donna Haraway A Cyborg Manifesto."
-	}}, 
-	{"question" : {
-		"synopsis": "The main trouble with cyborgs is that they are the illegitimate offspring of militarism and patriarchal capitalism, but illegitimate offspring are...",
-		"optionsA": ["open to read rest"],
-		"optionsN": ["no thanks"],
-		"lose_message": "often exceedingly unfaithful to their origins. Their fathers, after all, are inessential - Donna Haraway A Cyborg Manifesto."
-	}},
-	{"question" : {
-		"synopsis": "The main trouble with cyborgs is that they are the illegitimate offspring of militarism and patriarchal capitalism, but illegitimate offspring are...",
-		"optionsA": ["open to read rest"],
-		"optionsN": ["no thanks"],
-		"lose_message": "often exceedingly unfaithful to their origins. Their fathers, after all, are inessential - Donna Haraway A Cyborg Manifesto."
-	}}, 
-	{"question" : {
-		"synopsis": "The main trouble with cyborgs is that they are the illegitimate offspring of militarism and patriarchal capitalism, but illegitimate offspring are...",
-		"optionsA": ["open to read rest"],
-		"optionsN": ["no thanks"],
-		"lose_message": "often exceedingly unfaithful to their origins. Their fathers, after all, are inessential - Donna Haraway A Cyborg Manifesto."
-	}},
-	{"question" : {
-		"synopsis": "The main trouble with cyborgs is that they are the illegitimate offspring of militarism and patriarchal capitalism, but illegitimate offspring are...",
-		"optionsA": ["open to read rest"],
-		"optionsN": ["no thanks"],
-		"lose_message": "often exceedingly unfaithful to their origins. Their fathers, after all, are inessential - Donna Haraway A Cyborg Manifesto."
-	}},
-	{"question" : {
-		"synopsis": "The main trouble with cyborgs is that they are the illegitimate offspring of militarism and patriarchal capitalism, but illegitimate offspring are...",
-		"optionsA": ["open to read rest"],
-		"optionsN": ["no thanks"],
-		"lose_message": "often exceedingly unfaithful to their origins. Their fathers, after all, are inessential - Donna Haraway A Cyborg Manifesto."
-	}},
-]
+	function loadQuestion() {
+		var answer = statements_lose_version[question_num]["question"]["synopsis"]; //is a string
+		var optionsA = statements_lose_version[question_num]["question"]["optionsA"];// options is a list 
+		var optionsN = statements_lose_version[question_num]["question"]["optionsN"];// options is a list 
+		console.log(optionsA)
+		$question.text(answer);
+		$optionsA.empty();
+		$optionsN.empty();
+		$playAgain.empty();
+		$loseMessage.empty();
+		$urnOpening.empty();
 
-var question_num = 0;
-
-var $question = $('#question_synopsis');
-var $optionsA = $('#optionsA');
-var $optionsN = $('#optionsN');
-var $playAgain = $("#play_again");
-var $loseMessage = $("#lose_message");
-var $urnOpening = $("#urn_opening")
-var $options = $('#options');
-
-$playAgain.click(function() {
-	incrementQuestionNum();
-	loadQuestion()
-});
-
-$optionsN.click(function() {
-	incrementQuestionNum();
-	loadQuestion()
-});
-
-$optionsA.click(function(){
-	$question.empty();
-	$optionsA.empty();
-	$optionsN.empty();
-	var lose_message = statements_lose_version[question_num]["question"]["lose_message"];// is a string
-	$loseMessage.text(lose_message);
-	$urnOpening.text("urn opening...");
-	$playAgain.text("play again");
-});
-
-
-function loadQuestion() {
-	var answer = statements_lose_version[question_num]["question"]["synopsis"]; //is a string
-	var optionsA = statements_lose_version[question_num]["question"]["optionsA"];// options is a list 
-	var optionsN = statements_lose_version[question_num]["question"]["optionsN"];// options is a list 
-	console.log(optionsA)
-	$question.text(answer);
-	$optionsA.empty();
-	$optionsN.empty();
-	$playAgain.empty();
-	$loseMessage.empty();
-	$urnOpening.empty();
-
-	var num = 0;
-	for (num= 0; num<optionsA.length; num++){	
-		var enter_negative = optionsA[num];
-		console.log(enter_negative)
-		$optionsA.append('<li>'+enter_negative+'</li>');
-		console.log($optionsA)
-	} 
-	for(var num1=0; num1<optionsN.length; num1++){	
-		var enter_affirmative = optionsN[num1];
-		$optionsN.append('<li>'+enter_affirmative+'</li>');
+		var num = 0;
+		for (num= 0; num<optionsA.length; num++){	
+			var enter_negative = optionsA[num];
+			console.log(enter_negative)
+			$optionsA.append('<li>'+enter_negative+'</li>');
+			console.log($optionsA)
+		} 
+		for(var num1=0; num1<optionsN.length; num1++){	
+			var enter_affirmative = optionsN[num1];
+			$optionsN.append('<li>'+enter_affirmative+'</li>');
+		}
 	}
+
+	function incrementQuestionNum() {
+		question_num += 1;
+		question_num = question_num % statements_lose_version.length;
+	}
+
+	loadQuestion();
 }
 
-function incrementQuestionNum() {
-	question_num += 1;
-	question_num = question_num % statements_lose_version.length;
-}
 
-loadQuestion();
  
