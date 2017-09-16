f9833277abd00ee93524c1c16d7d70f412e40fa3
withquietintentions <Zoe@penelope.local>
2017-09-15 20:50:46 -0700

    update blinker

--- app.js
+++ app.js
@@ -19,47 +19,17 @@ app.use(cors());
 //NodeJS code for using Photon variable
 var Particle = require('particle-api-js');
 var particle = new Particle();
-var token = process.env.TOKEN;
-var d_uid = process.env.D_UID;
+const TOKEN = process.env.TOKEN;
+const D_UID = process.env.D_UID;
 
 
-var timer1;
-
-timer1 = setInterval(talk_with_photon,10000);//might want the convo only happen on an event? 
-
-function talk_with_photon() {
-    g_var(d_uid, token);
-    c_func(d_uid, token);          
-}
-
-function g_var(d_id,l_token){
-    particle.getVariable({ deviceId: d_id, name: 'int_val', auth: l_token }).then(
-        function (data) {
-            console.log('Device variable retrieved successfully:', data.body.result);
-        }, function (err) {
-            console.log('An error occurred while getting attrs:', err);
-        });
-}
-
-function c_func(d_id, l_token) {
-
-    var fnPr = particle.callFunction({ deviceId: d_id, name: 'int_fun', argument: 'wifi_rssi', auth: l_token });
-
-    fnPr.then(
-        function (data) {
-            console.log('Function called succesfully WiFi_RSSI: ', data.body.return_value);
-        }, function (err) {
-            console.log('An error occurred:', err);
-        });
-        
-    var fnPr2 = particle.callFunction({ deviceId: d_id, name: 'int_fun', argument: 'blink', auth: l_token });
-
-    fnPr2.then(
-        function (data) {
-            console.log('Function called succesfully LED: ', data.body.return_value);
-        }, function (err) {
-            console.log('An error occurred:', err);
-        });
+function particle_blink() {
+    particle.callFunction({ deviceId: D_UID, name: 'blink', auth: TOKEN }).then(
+	    function (data) {
+	        console.log('Blink called succesfully : ', data.body.return_value);
+	    }, function (err) {
+	        console.log('An error occurred:', err);
+	    });	
 }
 ///END OF PARICLE TEST
 
@@ -86,6 +56,9 @@ app.post("/guess", function(req, res) {
         if (result != null) {
           console.log("removed!: ", result);
           // if result is non null then there is a match
+          // MATCH WAS FOUND!!!!!!!!!!!!
+         particle_blink();
+
           res.send(guess);
         } else {
           // if no match
