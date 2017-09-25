7fdff2019d9a8c68ac94003e24a538ccc72bde04
withquietintentions <Zoe@penelope.local>
2017-09-24 21:14:42 -0700

    enter key  on keboard now works

--- public/js/main.js
+++ public/js/main.js
@@ -5,3 +5,14 @@ $(document).ready(function(){
 	$('#guess').keyboard({ layout: 'qwerty' });
 	//.addTyping();
 })
+
+
+// MAKES enter KEY WORK!
+$.keyboard.keyaction.enter = function(base){
+  if (base.el.tagName === "INPUT") {
+    base.accept();      // accept the content
+    $('form').submit(); // submit form on enter
+  } else {
+    base.insertText('\r\n'); // textarea
+  }
+};
