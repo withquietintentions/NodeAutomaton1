12d27f7ba700e5ab16c614a5dc1be4a9373d1970
withquietintentions <Zoe@penelope.local>
2017-09-25 23:07:23 -0700

    added a fade in and fade out of response message

--- public/js/dictionary.js
+++ public/js/dictionary.js
@@ -1,5 +1,6 @@
 
 //javascript
+
 var dictionary = {
   onRemove: function(ev) {
     var guess = $("#guess").val();
@@ -11,9 +12,13 @@ var dictionary = {
       .done(function(data) {
         console.log("data", data);
         if (data && data.length > 0) {
-          messageHtml.html(guess + " was removed from the world, thank you.");
+          messageHtml.fadeIn();
+          messageHtml.html(guess + " was removed from the world, thank you.")
+          messageHtml.fadeOut(7000);
         } else {
-          messageHtml.html("Sorry not enough people have requested that be removed from the world yet.");
+           messageHtml.fadeIn();
+          messageHtml.html("Sorry not enough ppl requested to remove that yet.");
+          messageHtml.fadeOut(7000);
         } 
       });;
   }
